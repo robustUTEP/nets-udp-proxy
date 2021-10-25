274e14573baffcd1ea6fc8ebfa4eb2fc2f167b7a
Eric Freudenthal <eric@freudenthal.net>
2021-10-25 18:32:18 -0400

    cleaned up symbols, added a few comments

--- udpServer.py
+++ udpServer.py
@@ -1,4 +1,4 @@
-#! /bin/python
+#! /usr/bin/env python3
 from socket import *
 
 # default params
@@ -6,7 +6,7 @@ serverAddr = ("", 50001)
 
 import sys
 def usage():
-    print "usage: %s [--serverPort <port>]"  % sys.argv[0]
+    print("usage: %s [--serverPort <port>]"  % sys.argv[0])
     sys.exit(1)
 
 try:
@@ -16,20 +16,20 @@ try:
         if sw == "--serverPort":
             serverAddr = ("", int(args[0])); del args[0]
         else:
-            print "unexpected parameter %s" % args[0]
+            print("unexpected parameter %s" % args[0])
             usage();
 except:
     usage()
 
-print "binding datagram socket to %s" % repr(serverAddr)
+print("binding datagram socket to %s" % repr(serverAddr))
 
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind(serverAddr)
-print "ready to receive"
+print("ready to receive")
 while 1:
     message, clientAddrPort = serverSocket.recvfrom(2048)
-    print "from %s: rec'd '%s'" % (repr(clientAddrPort), message)
-    modifiedMessage = message.upper()
-    serverSocket.sendto(modifiedMessage, clientAddrPort)
+    print("from %s: rec'd '%s'" % (repr(clientAddrPort), message))
+    modifiedMessage = message.decode().upper()
+    serverSocket.sendto(modifiedMessage.encode(), clientAddrPort)
     
                 
